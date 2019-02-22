d4010a0cac31fe93500819246847bc4e1cff51ca
Takashi Kato <ktakashi@ymail.com>
2019-02-22 15:25:22 +0100

    Forgot to add the test app

---
+++ test/web/filters.scm
@@ -0,0 +1,29 @@
+(library (web filters)
+    (export run
+	    filter0
+	    filter1
+	    filter2)
+    (import (rnrs)
+	    (sagittarius nginx))
+
+(define (run request response)
+  (put-bytevector (nginx-response-output-port response)
+		  (string->utf8
+		   (if (nginx-request-peer-certificate request)
+		       "secure"
+		       "Not secure")))
+  (values 200 'text/plain))
+
+(define (filter0 request response next)
+  (put-bytevector (nginx-response-output-port response)
+		  (string->utf8 "filter 0\n"))
+  (next request response))
+(define (filter1 request response next)
+  (put-bytevector (nginx-response-output-port response)
+		  (string->utf8 "filter 1\n"))
+  (next request response))
+(define (filter2 request response next)
+  (put-bytevector (nginx-response-output-port response)
+		  (string->utf8 "filter 2\n"))
+  (next request response))
+)
